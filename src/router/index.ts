ae0430229716341b8e352969921d36b31dec02c0
t_inoue <t.artwork.ceramica@gmail.com>
2023-06-18 17:50:54 +0900

    modify: routing files

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,30 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// ルーティング設定を行うファイル
+// src/main.ts に import されている
+import { createRouter, createWebHistory } from "vue-router";
+import type { RouteRecordRaw } from "vue-router";
+import AppTop from "@/views/AppTop.vue";
+// import HomeView from "../views/HomeView.vue";
+
+const routeSettings: RouteRecordRaw[] = [{ path: "/", name: "AppTop", component: AppTop }];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+    history: createWebHistory(import.meta.env.BASE_URL),
+    // routes: [
+    //     {
+    //         path: "/",
+    //         name: "home",
+    //         component: HomeView
+    //     },
+    //     {
+    //         path: "/about",
+    //         name: "about",
+    //         // route level code-splitting
+    //         // this generates a separate chunk (About.[hash].js) for this route
+    //         // which is lazy-loaded when the route is visited.
+    //         component: () => import("../views/AboutView.vue")
+    //     }
+    // ]
+    routes: routeSettings
+});
 
-export default router
+export default router;
