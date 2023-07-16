32063c65b1805b9179e333c492951d9bf68c9c44
t_inoue <t.artwork.ceramica@gmail.com>
2023-07-16 22:13:58 +0900

    fix(11.1-Pinia-基本): 3. ストアファイル内記述の基本構造
    counter.ts 内の基本構文

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,12 +1,32 @@
 import { ref, computed } from "vue";
-import { defineStore } from "pinia";
+import { defineStore } from "pinia"; // defineStore() 関数をインポート
 
-export const useCounterStore = defineStore("counter", () => {
-    const count = ref(0);
-    const doubleCount = computed(() => count.value * 2);
-    function increment() {
-        count.value++;
+// useCounterStore を export
+export const useCounterStore = defineStore({
+    id: "counter", // ストア名
+    state: () => ({
+        // データ本体(オブジェクト)
+        counter: 0
+    }),
+    getters: {
+        // データの出力時の加工処理
+        doubleCount: (state) => state.counter * 2
+    },
+    actions: {
+        // 保持データの変更処理
+        increment() {
+            this.counter++;
+        }
     }
-
-    return { count, doubleCount, increment };
 });
+
+// 元からある useCounterStore
+// export const useCounterStore = defineStore("counter", () => {
+//     const count = ref(0);
+//     const doubleCount = computed(() => count.value * 2);
+//     function increment() {
+//         count.value++;
+//     }
+
+//     return { count, doubleCount, increment };
+// });
